87388b9824376ffa2c49de0cb4aa78cf617dc4dd
Emre Can Şahin <108821600+chimerasep@users.noreply.github.com>
2024-10-22 05:09:20 +0300

    Update barracudadeneme.cs

--- barracudadeneme.cs
+++ barracudadeneme.cs
@@ -1,1 +1,44 @@
+using Unity.Barracuda;
+using UnityEngine;
+
+public class ImageProcessor : MonoBehaviour
+{
+    public NNModel modelAsset;
+    private IWorker worker;
+    private Texture2D inputTexture;
+    private RenderTexture outputTexture;
+
+    void Start()
+    {
+        Model model = ModelLoader.Load(modelAsset);
+        worker = WorkerFactory.CreateWorker(WorkerFactory.Type.Compute, model);
+
+       
+        inputTexture = new Texture2D(256, 256); 
+        outputTexture = new RenderTexture(256, 256, 0);
+    }
+
+    public void ProcessImage(Texture2D inputImage)
+    {
+        // convert to tensor
+        Tensor inputTensor = new Tensor(inputImage, 3); 
+        worker.Execute(inputTensor);
+
+        
+        Tensor outputTensor = worker.PeekOutput();
+
+        // back to texture
+        RenderTexture.active = outputTexture;
+        outputTensor.ToRenderTexture(outputTexture);
+        RenderTexture.active = null;
+
+        inputTensor.Dispose();
+        outputTensor.Dispose();
+    }
+
+    void OnDestroy()
+    {
+        worker.Dispose();
+    }
+}
 
